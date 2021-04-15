cb21aebf0cf475367e803d1a48545fde29782d52
Nik Quito <nickquitoespinoza@gmail.com>
2021-04-15 16:18:00 -0700

    Initial Commit

---
+++ SimuladorTarjetas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimuladorTarjetas
+//
+//  Created by nickthewitcher on 4/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
