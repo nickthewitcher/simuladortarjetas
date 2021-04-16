8e253b60cd66e11b5fa693b85540afa4c7d8d423
Nik Quito <nickquitoespinoza@gmail.com>
2021-04-16 05:01:58 -0700

    2da pantalla

---
+++ SimuladorTarjetas/SecondController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondController.swift
+//  SimuladorTarjetas
+//
+//  Created by nickthewitcher on 4/16/21.
+//
+
+import Foundation
+import Foundation
+import UIKit
+
+class SecondController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("mostrar 2da")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
