59cabd94281f0dc49e8761e1e3d979488def387a
Nik Quito <nickquitoespinoza@gmail.com>
2021-04-15 21:36:41 -0700

    actualizado picker views

--- SimuladorTarjetas/ViewController.swift
+++ SimuladorTarjetas/ViewController.swift
@@ -9,23 +9,49 @@ import UIKit
 import Alamofire
 import RxSwift
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
  
-
-    @IBOutlet weak var cardPicker: UIPickerView!
+    
+    @IBOutlet weak var dniTextField: UITextField!
+    
+    
+    @IBOutlet weak var tipoTarjetaTextField: UITextField!
+    
+    
+    @IBOutlet weak var montoTextField: UITextField!
+    
+    
+    @IBOutlet weak var cuotasTextField: UITextField!
+    
+    
+    
+    @IBOutlet weak var tasasTextField: UITextField!
+    
+    
+    @IBOutlet weak var diaPagoTextField: UITextField!
+    
+     
+    
+    
+    var cardPicker = UIPickerView()
+    var cuotasPicker = UIPickerView()
+    var tasasPicker = UIPickerView()
+    var pagosPicker = UIPickerView()
+    
+    // var cardPicker: UIPickerView
     var cardPickerData: [String] = [String]()
     var cardOrdenElegido: Int = 0
     
-    @IBOutlet weak var cuotasPicker: UIPickerView!
+    // var cuotasPicker: UIPickerView
     var cuotasPickerData: [String] = [String]()
     var cuotasOrdenElegido: Int = 0
     
     
-    @IBOutlet weak var tasasPicker: UIPickerView!
+    // var tasasPicker: UIPickerView
     var tasasPickerData: [String] = [String]()
     var tasasOrdenElegido: Int = 0
     
-    @IBOutlet weak var pagosPicker: UIPickerView!
+    // var pagosPicker: UIPickerView
     var pagosPickerData: [String] = [String]()
     var pagosOrdenElegido: Int = 0
     
@@ -34,7 +60,17 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-
+       /* let cardPicker = UIPickerView()
+        let cuotasPicker = UIPickerView()
+        let tasasPicker = UIPickerView()
+        let pagosPicker = UIPickerView()
+*/
+        
+        self.cardPicker.tag = 0
+        self.cuotasPicker.tag = 1
+        self.tasasPicker.tag = 2
+        self.pagosPicker.tag = 3
+        
         // Connect data:
         self.cardPicker.delegate = self
         self.cardPicker.dataSource = self
@@ -47,10 +83,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
         
         self.pagosPicker.delegate = self
         self.pagosPicker.delegate = self
-               
+        
+        self.tipoTarjetaTextField.inputView = self.cardPicker
+        
+        self.cuotasTextField.inputView = self.cuotasPicker
+        
+        self.tasasTextField.inputView = self.tasasPicker
+        
+        self.diaPagoTextField.inputView = self.pagosPicker
+        
+        
         
         let apiService = ApiService.sharedInstance
-         apiService.getSimulator()
+        _ = apiService.getSimulator()
             .subscribe(
                 onNext: {
                     print("onNext: \($0)")
@@ -61,6 +106,11 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                     self.tasasPickerData = ($0).response.tea
                     
                     self.pagosPickerData = ($0).response.diasPagos
+                    
+                    self.tipoTarjetaTextField.text = self.cardPickerData[0]
+                    self.cuotasTextField.text = self.cuotasPickerData[0]
+                    self.tasasTextField.text = self.tasasPickerData[0]
+                    self.diaPagoTextField.text = self.pagosPickerData[0]
                     
                     DispatchQueue.main.async {
                         self.cardPicker.reloadComponent(0)
@@ -123,18 +173,31 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
                 case 0: print("card fila elegida ", row)
                     print("card componente elegido ",component)
                     self.cardOrdenElegido = row
+                    self.tipoTarjetaTextField.text = self.cardPickerData[row]
+                    self.view.endEditing(true)
+
                 case 1:
                     print("cuotas fila elegida ", row)
                     print("cuotas componente elegido ",component)
                     self.cuotasOrdenElegido = row
+                    self.cuotasTextField.text = self.cuotasPickerData[row]
+                    self.view.endEditing(true)
+
                 case 2:
                     print("tasas fila elegida ", row)
                     print("tasas componente elegido ",component)
                     self.tasasOrdenElegido = row
+                    self.tasasTextField.text = self.tasasPickerData[row]
+                    self.view.endEditing(true)
+
                 case 3:
                     print("pagos fila elegida ", row)
                     print("pagos componente elegido ",component)
                     self.pagosOrdenElegido = row
+                    self.diaPagoTextField.text = self.pagosPickerData[row]
+                    self.view.endEditing(true)
+
+                    
                 default: return
                 }
       }
